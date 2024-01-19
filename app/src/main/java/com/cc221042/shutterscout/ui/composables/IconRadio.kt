3fda70d3b89e8fdec6dc66f84952e2ceaea7e5ea
gabrielmermer <zubrzysta@gmail.com>
2024-01-19 14:06:55 +0100

    working selecting icons - slow and not plugged to backend yet

---
+++ app/src/main/java/com/cc221042/shutterscout/ui/composables/IconRadio.kt
@@ -0,0 +1,55 @@
+package com.cc221042.shutterscout.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cc221042.shutterscout.R
+
+@Composable
+fun IconRadio(name: String, isSelected: Boolean, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .width(60.dp)
+            .height(60.dp)
+            .background(Color(0xFFFFFFFF), shape = RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .border(
+                width = if (isSelected) 2.dp else 0.dp,
+                color = if (isSelected) Color(0xFF127FBF)else Color.Transparent,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = name,
+            style = TextStyle(
+                fontSize = 22.sp,
+                lineHeight = 28.sp,
+                fontFamily = FontFamily(Font(R.font.font_awesome)),
+                fontWeight = FontWeight(900),
+                color = if (isSelected) Color(0xFF127FBF) else Color(0xFF9E9E9E),
+                textAlign = TextAlign.Center,
+            )
+        )
+    }
+}
--- app/src/main/java/com/cc221042/shutterscout/ui/screens/AddPlaceScreen.kt
+++ app/src/main/java/com/cc221042/shutterscout/ui/screens/AddPlaceScreen.kt
@@ -3,15 +3,23 @@ package com.cc221042.shutterscout.ui.screens
 import android.net.Uri
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,20 +31,30 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cc221042.shutterscout.Place
+import com.cc221042.shutterscout.R
 import com.cc221042.shutterscout.data.getLatLongFromImageUri
+import com.cc221042.shutterscout.ui.Corner
 import com.cc221042.shutterscout.ui.MainViewModel
 import com.cc221042.shutterscout.ui.composables.setupPhotoPicker
+import com.cc221042.shutterscout.ui.gradientBackground
+import androidx.compose.ui.geometry.Offset
+import com.cc221042.shutterscout.ui.composables.IconRadio
 
 @Composable
 fun AddPlaceScreen(mainViewModel: MainViewModel) {
-    var title by rememberSaveable { mutableStateOf("") }
+    var title by rememberSaveable { mutableStateOf("")}
     var condition by rememberSaveable { mutableStateOf("") }
     var imageUri by rememberSaveable { mutableStateOf("") }
     var saveSuccess by remember { mutableStateOf(false) }
@@ -46,6 +64,9 @@ fun AddPlaceScreen(mainViewModel: MainViewModel) {
     var longitude by rememberSaveable { mutableStateOf<Double?>(null) }
     val context = LocalContext.current
 
+    // map icon
+    var icon by rememberSaveable { mutableStateOf("") }
+
     val photoPicker = setupPhotoPicker { uri: Uri ->
         imageUri = uri.toString()
         val latLong = getLatLongFromImageUri(context, uri)
@@ -53,59 +74,222 @@ fun AddPlaceScreen(mainViewModel: MainViewModel) {
         longitude = latLong.second
     }
 
-    Scaffold(
+    val gradientColors = listOf(Color(0xFFDE911D), Color(0xFFF0B429))
 
+    val diagonalGradientBrush = Brush.linearGradient(
+        colors = gradientColors,
+        start = Offset(0f, 0f), // Top left corner
+        end = Offset(100f, 100f) // Adjust for the desired diagonal effect
+    )
+
+    Scaffold(
+        containerColor = Color(0xFFF7F7F7),
     ) {innerPadding ->
+
+        // Go back box
+        Box(modifier = Modifier.fillMaxSize()) { // Full-screen canvas
+            Box(
+                modifier = Modifier
+                    .offset(x = 2.dp, y = 10.dp) // Positioning offset
+                    .width(48.dp)
+                    .height(48.dp)
+                    .background(Color.Yellow),
+                contentAlignment = Alignment.Center // Ensures the chevron is centered in the box
+            ) {
+                Text(
+                    text = "chevron-left",
+                    style = TextStyle(
+                        fontSize = 22.sp,
+                        lineHeight = 28.sp,
+                        fontFamily = FontFamily(Font(R.font.font_awesome)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF565656),
+                        textAlign = TextAlign.Center,
+                    ), modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState()),
 
-            verticalArrangement = Arrangement.Center,
+//            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "ShutterScout", fontSize = 50.sp, style = TextStyle(fontFamily = FontFamily.Cursive))
 
-            Spacer(modifier = Modifier.height(50.dp))
 
-            TextField(
-                value = title,
-                onValueChange = { newText -> title = newText },
-                label = { Text("Title of your place") }
+            Text(
+                text = "Add a new place",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    lineHeight = 28.sp,
+                    fontFamily = FontFamily(Font(R.font.lexend)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF565656),
+                    textAlign = TextAlign.Center,
+                ), modifier = Modifier
+                    .padding(top = 20.dp)
             )
-            TextField(
-                value = condition,
-                onValueChange = { newText -> condition = newText },
-                label = { Text("Condition of your place") }
-            )
+            Box(
+                modifier = Modifier
+                    .padding(top = 38.dp)
+                    .padding(horizontal = 12.dp)
+                    .gradientBackground(
+                        colors = listOf(Color(0xFFDE911D), Color(0xFFF0B429)),
+                        cornerRadius = 10.dp,
+                        startCorner = Corner.TOP_LEFT,
+                        endCorner = Corner.BOTTOM_RIGHT
+                    )
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Button(
-                onClick = { photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) },
-                modifier = Modifier.padding(top = 10.dp)
             ) {
-                Text("Open Gallery", fontSize = 20.sp)
-            }
+                Button(
+                    onClick = { photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) },
+                    shape = RoundedCornerShape(10.dp),
+                    // TODO add gradient
+                    colors = ButtonDefaults.buttonColors(Color(0xFFF0B429)),
+                    modifier = Modifier
+//                        .padding(top = 38.dp)
+//                        .padding(horizontal = 12.dp)
+                        .fillMaxWidth()
+                        .height(138.dp)
 
-            Button(
-                onClick = {
-                    mainViewModel.save(Place(title, condition, imageUri, latitude, longitude))
-                    saveSuccess = true // Update the state to reflect save success
-                },
-                modifier = Modifier.padding(top = 15.dp),
-                enabled = title.isNotBlank() && condition.isNotBlank()
-            ) {
-                Text("Save", fontSize = 20.sp)
-            }
 
-            if (saveSuccess) {
-                Text("Place saved successfully!", color = Color.Green)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+
+                        ) {
+                        Text(
+                            text = "plus-circle",
+                            style = TextStyle(
+                                fontSize = 22.sp,
+                                lineHeight = 28.sp,
+                                fontFamily = FontFamily(Font(R.font.font_awesome)),
+                                fontWeight = FontWeight(400),
+                                color = Color(0xFFFFFFFF),
+                                textAlign = TextAlign.Center,
+                            )
+                        )
+                        Text(
+                            text = "add picture",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight(400),
+                                color = Color(0xFFFFFFFF),
+                                textAlign = TextAlign.Center,
+                            ), modifier = Modifier.padding(top = 4.dp)
+                        )
+                    }
+                }
             }
         }
 
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+
+
+            Column(
+                modifier = Modifier
+                    .padding(top = 244.dp, start = 12.dp)
+
+            ) {
+                Text(
+                    text = "Name",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF565656),
+                    ), modifier = Modifier
+//                    .padding(top = 244.dp, start = 12.dp)
+                )
+
+
+                TextField(
+                    value = title,
+                    onValueChange = { newText -> title = newText },
+                    label = { null }
+                )
+
+                Text(
+                    text = "Icon on map",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.lexend)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF565656),
+                    ), modifier = Modifier
+                    .padding(top = 20.dp, bottom = 12.dp)
+                )
+
+                // Define your icons
+                val icons = listOf("map-marker-alt", "star", "heart", "mountain", "building", "university", "water", "tree")
+
+                // Split icons into two rows
+                val firstRowIcons = icons.take(4)
+                val secondRowIcons = icons.drop(4)
+
+                // First row of icons
+                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                    firstRowIcons.forEach { iconName ->
+                        IconRadio(
+                            name = iconName,
+                            isSelected = icon == iconName,
+                            onClick = { icon = iconName }
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp)) // Space between rows
+
+                // Second row of icons
+                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                    secondRowIcons.forEach { iconName ->
+                        IconRadio(
+                            name = iconName,
+                            isSelected = icon == iconName,
+                            onClick = { icon = iconName }
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                    }
+                }
+                Spacer(modifier = Modifier.height(200.dp))
+                TextField(
+                    value = condition,
+                    onValueChange = { newText -> condition = newText },
+                    label = { Text("Condition of your place") }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+
+
+                Button(
+                    onClick = {
+                        mainViewModel.save(Place(title, condition, imageUri, latitude, longitude))
+                        saveSuccess = true // Update the state to reflect save success
+                    },
+                    modifier = Modifier.padding(top = 15.dp),
+                    enabled = title.isNotBlank() && condition.isNotBlank()
+                ) {
+                    Text("Save", fontSize = 20.sp)
+                }
+
+                if (saveSuccess) {
+                    Text("Place saved successfully!", color = Color.Green)
+                }
+            }
+        }
+
+
+
     }
 
-
-}
